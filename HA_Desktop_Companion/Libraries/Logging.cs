b58858b7d929f0f9ac9adf1052368a6461434717
JonatanRek <gamerclassn7@gmail.com>
2022-08-25 14:38:34 +0200

    Do not lock Log.txt on loging alow opening in difrent editor

--- HA_Desktop_Companion/Libraries/Logging.cs
+++ HA_Desktop_Companion/Libraries/Logging.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
 
 namespace HA_Desktop_Companion.Libraries {
     public class Logging {
         public string logFile;
         private StreamWriter sw;
         public Logging(string logFilePath) {
-            sw = new StreamWriter(logFilePath, true);
+            FileStream fs = File.Open(logFilePath, FileMode.Append, FileAccess.Read, FileShare.ReadWrite);
+            sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
         }
 
         public void Write(string msg)
