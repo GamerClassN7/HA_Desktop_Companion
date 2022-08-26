08e4cb1ff2de20301979ae1f0665ef439a63a752
JonatanRek <gamerclassn7@gmail.com>
2022-08-26 12:37:45 +0200

    Self update mechanics

---
+++ GH_Updater/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GH_Updater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+             if (e.Args.Length == 1)
+                wnd.checkForUpdates(e.Args[0], e.Args[0]);
+
+            wnd.Show();
+        }
+    }
+}
