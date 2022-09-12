6339d4ff3aa37c0408731f494aa59210f52c01a3
JonatanRek <gamerclassn7@gmail.com>
2022-09-12 08:45:05 +0200

    Basic Implementation of Perf COunter should be later used for beter CPU and ram monitoring

--- Sensors_Test_Ground/Program.cs
+++ Sensors_Test_Ground/Program.cs
@@ -1,16 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Win32;
+using System.Diagnostics;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
 
 Console.WriteLine("Hello, World!");
 
 
+string name = "Processor";
+string category = "% Processor Time";
+string instance = "_Total";
+int maxSampleCount = 3;
 
-string[] rebootRequiredKeys = {
-            @"Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" ,
-            @"SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired",
-            @"SYSTEM\CurrentControlSet\Control\Session Manager"
-};
+Console.WriteLine(rqueryPerfCounter(name, category, instance, maxSampleCount));
+Console.WriteLine();
 
+static float rqueryPerfCounter(string name, string category, string instance, int maxSampleCount)
+{
+    try
+    {
+        PerformanceCounter total_cpu = new(name, category, instance);
 
+        maxSampleCount = 3;
+        int sampleCount = maxSampleCount;
 
+        float result = 0;
+        while (sampleCount >= 0)
+        {
+            if (sampleCount != maxSampleCount)
+            {
+                result += total_cpu.NextValue();
+            }
+
+            sampleCount--;
+        }
+
+        return (result / maxSampleCount);
+    }
+    catch (Exception)
+    {
+
+        return 0.0f;
+    }
+}
