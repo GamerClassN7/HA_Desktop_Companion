9f2f27b46998fcaea0b2bd0e7843b08347250b18
JonatanRek <gamerclass7@gmail.com>
2023-04-11 19:45:03 +0200

    Next Step Location Provider Services Handling

---
+++ HA/Class/HomeAssistant/Objects/HomeAssistantLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HA.Class.HomeAssistant.Objects
+{
+    internal class HomeAssistantLocation
+    {
+        public double[] gps = { 0.00, 0.00};
+        public int gps_accuracy = 0;
+    }
+}
---
+++ HA/Class/Sensors/IpLocation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HA.Class.Sensors
+{
+    internal class IpLocation
+    {
+        public static string getData()
+        {
+            try
+            {
+                string info = new WebClient().DownloadString("http://ipinfo.io/");
+                return (string)JsonSerializer.Deserialize<JsonObject>(info)["loc"];
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
