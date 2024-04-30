eeb12a7c12b594f006672d71e4a6f4e24c336842
GamerClassN7 <gamerclassn7@gmail.com>
2024-04-30 05:10:34 -0700

    Create Logger.cs

---
+++ HADC_REBORN/Class/Helpers/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HADC_REBORN.Class.Helpers
+{
+    internal class Logger
+    {
+        private static bool isInistialized = false;
+#if DEBUG
+        private string appDir = Directory.GetCurrentDirectory();
+#else
+        private string appDir = AppDomain.CurrentDomain.BaseDirectory();
+#endif
+        private string logFilePat
+    }
+}
