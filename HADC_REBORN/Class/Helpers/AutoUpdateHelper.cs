855a72fae4cf2dc15d16d274678415f5fee1bbd0
JonatanRek <gamerclassn7@gmail.com>
2025-02-13 12:58:02 +0100

    Autoupdater cherry pick

---
+++ HADC_REBORN/Class/Helpers/AutoUpdateHelper.cs
@@ -0,0 +1,37 @@
+﻿using AutoUpdaterDotNET;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HADC_REBORN.Class.Helpers
+{
+    internal class AutoUpdateHelper
+    {
+        public AutoUpdateHelper()
+        {
+            AutoUpdater.ParseUpdateInfoEvent += AutoUpdaterOnParseUpdateInfoEvent;
+            AutoUpdater.Synchronous = true;
+            AutoUpdater.ShowRemindLaterButton = false;
+            AutoUpdater.ClearAppDirectory = false;
+            //AutoUpdater.ReportErrors = Debugger.IsAttached;
+            AutoUpdater.HttpUserAgent = ("FakeDeck-v" + Assembly.GetExecutingAssembly().GetName().Version);
+            AutoUpdater.Start("https://github.com/GamerClassN7/HA_Desktop_Companion/releases/latest/download/meta.xml");
+        }
+
+        private void AutoUpdaterOnParseUpdateInfoEvent(ParseUpdateInfoEventArgs args)
+        {
+            JsonElement json = JsonDocument.Parse(args.RemoteData).RootElement;
+            args.UpdateInfo = new UpdateInfoEventArgs
+            {
+                CurrentVersion = json.GetProperty("tag_name").ToString().TrimStart('v') + ".0",
+                DownloadURL = json.GetProperty("zipball_url").ToString(),
+            };
+            Debug.WriteLine("calling Updater");
+        }
+    }
+}
